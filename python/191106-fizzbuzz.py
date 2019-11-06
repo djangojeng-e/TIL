5f528b5832206aae3fe7a350755bfa3c7f4c7bf8
djangojeng-e <headfat1218@gmail.com>
2019-11-06 17:40:17 +1100

    feat
    List comprehension code has been suggested.
    Refer to the ## comment areas for codes in list comprehension format.

--- python/191106-fizzbuzz.py
+++ python/191106-fizzbuzz.py
@@ -13,11 +13,17 @@ for i in range(1,101):
     else:
         print(i)
 
-
+# operator with bool -> It can be 0 or 1. 
 # for i in range(1,100+1):
 #   if i%3==0 or i%5==0: 
 #    print("fizz"*(i%3==0) + "buzz"*(i%5==0)
     #else: 
         #print(i)
+
+
+# Fizzbuzz in List comprehension
+
+# ["fizz buzz" if i%15==0 else "fizz" if i%3==0 else "buzz" if i%5==0 else i for i in range(0,101)]
+
 
 
