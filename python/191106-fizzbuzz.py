c9642d1f5f8d3ad86d1d5f1459c9f3d54e2c28a6
djangojeng-e <headfat1218@gmail.com>
2019-11-06 20:49:04 +1100

    feat
    the code has been updated using the method required by second condition
    (Using 1 loop condition with one conditional statement)

--- python/191106-fizzbuzz.py
+++ python/191106-fizzbuzz.py
@@ -18,4 +18,10 @@ for i in range(0, 101):
         print("fizz")
 
 
+# 2 
 
+for i in range(0, 101):
+    if i % 3 == 0 or i % 5 == 0: 
+        print("fizz"*(i % 3 == 0) + "buzz"*(i % 5 == 0))
+    else:
+        print(i)
