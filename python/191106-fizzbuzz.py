7d21b5bba512b7ec4b8b3b5f5d8c71bc9b050e56
djangojeng-e <headfat1218@gmail.com>
2019-11-06 20:41:57 +1100

    feat
    The exercise has been updated into the file.
    Write the code according to the requirement described in the file.

--- python/191106-fizzbuzz.py
+++ python/191106-fizzbuzz.py
@@ -1,2 +1,11 @@
 
+# fizzbuzz exercise 
+# for the numbers between 0 to 100, print "fizz" if the number 
+# is divisible by 3 and print "buzz" if the number is divisible by 5
+# if the number is divisible by 15, print the whole word "fizzbuzz". 
+
+# 1. execute the above using 1 loop statement with conditional statements  
+# 2. execute the above using 1 loop statement with one conditional statement 
+# 3. create the list containing the result by exploiting list comprehesion method. 
+
 
