985450c1cd16a0c3281271cbd7d5bbdf71fff659
djangojeng-e <headfat1218@gmail.com>
2019-11-13 19:05:00 +0900

    solution has been updated and tested fine.

---
+++ exercises/exercise1.py
@@ -0,0 +1,12 @@
+name = input("Please enter your name.") 
+age = int( input("Please enter your current age.")) 
+current_year = int(input("Please enter the current calendar year.")) 
+years_to_hundred = (100 - age) 
+year_turn_hundred = current_year + years_to_hundred
+
+print("You turn into 100 years old after %d years" % years_to_hundred) 
+print("The year you turn 100 years will be %d." % year_turn_hundred)
+
+
+
+
