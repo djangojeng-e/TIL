1e3bde5a1496ae4301ce5a8203f88ae23c451a34
djangojeng-e <headfat1218@gmail.com>
2019-11-14 20:41:01 +0900

    feat: Solution has been updated!

---
+++ exercises/exercise2.py
@@ -0,0 +1,20 @@
+#1. 
+num1 = int(input("Please enter your number!"))
+
+if num1 % 2 == 0:
+    print("Your number is EVEN number!")
+else:
+    print("Your number is ODD number!")
+
+#2. 
+
+num2 = int(input("Please enter second number!"))
+num3 = int(input("Please enter third number!"))
+
+if num2 % num3 == 0:
+    print("second number is divisible by third number perfectly!")
+else:
+    print("Second number is not divisible by third number!")
+
+
+
