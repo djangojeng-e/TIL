28c852561a7d42eea8f3e4e901550b396fc6a432
djangojeng-e <headfat1218@gmail.com>
2019-11-18 09:23:19 +0900

    feat: Solution for Question6 has been updated.

---
+++ exercises/exercise6.py
@@ -0,0 +1,10 @@
+string = input("Enter your string")
+
+string_reverse = string[-1::-1]
+
+if string_reverse == string:
+    print("Your string is a Palindrome.") 
+else:
+    print("Your string is not a Palindrome.") 
+
+
