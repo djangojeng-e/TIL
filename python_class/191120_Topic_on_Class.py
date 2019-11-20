70a8b74b29644eddfdf54b9ab05b2b322de3842f
djangojeng-e <headfat1218@gmail.com>
2019-11-20 20:32:55 +0900

    Added the first exmample to understand Instances

---
+++ python_class/191120_Topic_on_Class.py
@@ -0,0 +1,21 @@
+# Understanding Instance 
+
+# It is fairly important to understand Instances. 
+
+# This example might help understand instances. 
+
+list1 = [1, 2, 3] 
+list2 = [1, 2, 3] 
+
+
+if list1 is list1: 
+    print("Of Course! list1 and list1 is the same instance!") 
+
+if list1 == list2:
+    print("list1 and list2 values are the same") 
+    if list1 is list2: 
+        print("And list1 and list2 are the same instance") 
+    else:
+        print("However, list1 and list2 are different instances.") 
+
+
