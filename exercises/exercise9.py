4a5ccf8ab7b20d7aab09ebab6a95ee05fea83334
djangojeng-e <headfat1218@gmail.com>
2019-11-21 13:56:54 +0900

    Soulution for Question 9 has been updated.

---
+++ exercises/exercise9.py
@@ -0,0 +1,24 @@
+import random 
+
+
+tries = 0              
+
+while True:
+    generate_number = random.randint(1,9)
+    user_number = input("Please enter your number : ")
+    if user_number == 'exit':
+        break8
+    elif generate_number == int(user_number):
+        print("You have guessed exactly right number.")
+        tries += 1
+    elif generate_number < int(user_number):
+        print("You have guessed too low.")
+        tries += 1
+    elif generate_number > int(user_number):
+        print("You have guessed too high.")
+        tries += 1
+    
+
+print(tries)
+
+
