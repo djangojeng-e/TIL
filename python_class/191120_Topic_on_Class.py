e09d68b834eca7fa068330257dc47d4f36f490ab
djangojeng-e <headfat1218@gmail.com>
2019-11-22 17:19:12 +0900

    While still not understanding the class, Understood that methods are equivlant to functions attached within classes.

--- python_class/191120_Topic_on_Class.py
+++ python_class/191120_Topic_on_Class.py
@@ -18,4 +18,17 @@ if list1 == list2:
     else:
         print("However, list1 and list2 are different instances.") 
 
+print()
+print()
+print()
+print("------"*8)
+# The codes below inspect if list1 and list2 are "List Class". 
+
+
+list1 = list(range(10)) 
+list2 = [1 ,2, 3] 
+
+if isinstance(list1, list) and isinstance(list2, list):
+    print("List1 and List 2 are both 'List Class'.")
+
 
