da1985ba35ac7dc5818824b7aaca2fcb5c970e6f
djangojeng-e <headfat1218@gmail.com>
2019-11-30 12:10:53 +0900

    feat: Solution for Question 18 has been updated.

---
+++ exercises/exercise18.py
@@ -0,0 +1,15 @@
+ordered_list = [1, 2, 3, 4, 5]
+
+given_number = int(input("Please enter your given number : "))
+
+
+def find(ordered_list, given_number):
+    for element in ordered_list:
+        if element == given_number:
+            return True
+        return False
+
+
+
+
+
