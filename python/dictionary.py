5409c94f2788eb7ea9deca024eacad393be152a7
djangojeng-e <headfat1218@gmail.com>
2019-12-09 10:48:05 +0900

    Reviewed key methods of dictionary.

--- python/dictionary.py
+++ python/dictionary.py
@@ -69,3 +69,83 @@ if "model" in dictionary:
 # Checking the length of dictionary.
 print(len(dictionary))
 
+
+
+dictionary = {
+"name" : "django", 
+"model" : "web framework",
+"year" : 2019
+}
+
+# Add language as a new key and the value of this new key will be Python 
+
+dictionary["language"] = "Python"
+
+print(dictionary)
+
+
+
+# Pop() method removes the item with the specified key name 
+
+dictionary = {
+    "name": "Django",
+    "model": "Web Framework",
+    "Year" : 2019,
+}
+
+dictionary.pop("Year") 
+print(dictionary)
+
+
+
+# Use del keyword to remove the item with key name
+
+dictionary = {
+    "name": "Django",
+    "model": "Web Framework",
+    "year": 2019
+}
+
+del dictionary["name"]
+print(dictionary)
+
+
+
+dictionary = {
+    "name" : "Django",
+    "model" : "Web Framework", 
+    "year" : 2019 
+}
+
+dictionary.clear()
+print(dictionary)
+
+# Delete dictionary using del keywords
+
+dictionary = {
+    "name" : "Django",
+    "model" : "Web Framework", 
+    "year" : 2019 
+}
+
+del dictionary
+
+# print(dictionary)
+# del keyword removes dictionary completely. 
+
+# Copy dictionary using copy()
+
+dictionary = {
+    "name": "Django",
+    "model": "Web Framework",
+    "Year": 2019
+}
+
+dictionary_two = dictionary.copy()
+print(dictionary_two)
+
+
+# Copy dictionary using dict()
+
+copied_dictionary = dict(dictionary)
+print(copied_dictionary)
