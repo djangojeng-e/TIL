15db06a82cc67e2cdc7034d6dfb611ac3eef339d
djangojeng-e <headfat1218@gmail.com>
2019-12-20 00:37:02 +0900

    Today, I learned Many-to-Many database relationship in Django.

---
+++ django/models/many_to_many/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.db.models import Model
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=30)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
+
