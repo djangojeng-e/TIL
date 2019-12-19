4332fa5e76d8c3f158c85ba571c6346787526606
djangojeng-e <headfat1218@gmail.com>
2019-12-19 12:54:21 +0900

    Class Meta Option has been tested.

--- django/models/database_models/admin.py
+++ django/models/database_models/admin.py
@@ -1,3 +1,7 @@
+
 from django.contrib import admin
+from database_models.models import Ox
 
 # Register your models here.
+
+admin.site.register(Ox)
--- django/models/database_models/models.py
+++ django/models/database_models/models.py
@@ -101,3 +101,13 @@
 #     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 #     date_joined = models.DateField()
 #     invite_reason = models.CharField(max_length=64)
+
+from django.db import models
+
+
+class Ox(models.Model):
+    horn_length = models.IntegerField()
+
+    class Meta:
+        ordering = ["horn_length"]
+        verbose_name_plural = "뿔의 길이"
