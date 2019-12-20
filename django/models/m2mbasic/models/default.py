03fa1cbe0cd67ef5c08e9d013bd4e1e75e5069ea
djangojeng-e <headfat1218@gmail.com>
2019-12-21 02:27:52 +0900

    Today, I practiced Many-To-Many basic and repackaged model.

---
+++ django/models/m2mbasic/models/default.py
@@ -0,0 +1,45 @@
+from django.db import models
+#
+# # Create your models here.
+#
+# # An Artical can be published in multiple Publication obejcts.
+# # Publication has multiple Article objects
+#
+#
+#
+# class Publication(models.Model):
+#     title = models.CharField(max_length=30)
+#
+#     class Meta:
+#         ordering = ['title']
+#
+#     def __str__(self):
+#         return self.title
+#
+#
+# class Article(models.Model):
+#     headline = models.CharField(max_length=100)
+#     publications = models.ManyToManyField(Publication)
+#
+#     class Meta:
+#         ordering = ['headline']
+#
+#     def __str__(self):
+#         return self.headline
+#
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=30)
+    toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        return self.name
+
+
