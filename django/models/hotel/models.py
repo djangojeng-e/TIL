bebdcdce6be9e8bfa80b95554870d2c3f5e3e5ad
djangojeng-e <headfat1218@gmail.com>
2019-12-24 19:40:24 +0900

    Updated hotel for basic manytomany relationship.

--- django/models/hotel/models.py
+++ django/models/hotel/models.py
@@ -2,18 +2,20 @@
 #
 # # Create your models here.
 #
-#
-# class Feature(models.Model):
-#     name = models.CharField(max_length=30)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Room(models.Model):
-#     name = models.CharField(max_length=30)
-#     features = models.ManyToManyField(Feature)
-#
-#     def __str__(self):
-#         return self.name
-#
+
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class RoomFeatures(models.Model):
+    name = models.CharField(max_length=30)
+    features = models.ManyToManyField(Feature)
+
+    def __str__(self):
+        return self.name
+
