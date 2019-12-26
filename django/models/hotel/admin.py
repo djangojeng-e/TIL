cb024bbab1059ff58bc0579d2a370ee3b33d84ed
djangojeng-e <headfat1218@gmail.com>
2019-12-26 13:41:42 +0900

    Practiced, venue, event database model today.

--- django/models/hotel/admin.py
+++ django/models/hotel/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Venue, Event, MyclubUser
 # Register your models here.
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MyclubUser)
+class MyclubUserAdmin(admin.ModelAdmin):
+    pass
+
