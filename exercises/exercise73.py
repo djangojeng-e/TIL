92304bc9c29e139c99417d02d8122173bb9cdd57
djangojeng-e <headfat1218@gmail.com>
2020-01-25 00:03:50 +0900

    feat: Solution 73 has been updated.

---
+++ exercises/exercise73.py
@@ -0,0 +1,14 @@
+class American():
+    pass
+
+
+class NewYorker(American):
+    pass
+
+
+anAmenrican = American()
+aNewYorker = NewYorker()
+print(anAmenrican) # Print out an object
+print(aNewYorker)  # Print out an object
+
+
