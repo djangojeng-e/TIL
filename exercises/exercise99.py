9965e8dfd69820ccc44c64600bfa9fb206b0f98c
djangojeng-e <headfat1218@gmail.com>
2020-02-21 09:22:15 +0900

    feat: Solution for Question 99 has been updated.

---
+++ exercises/exercise99.py
@@ -0,0 +1,12 @@
+from random import shuffle
+
+li = [3, 6, 7, 8]
+
+shuffle(li)
+
+print(li)
+
+# Check the difference between shuffle(li)
+# And shuffle(li)
+new_list = shuffle(li)
+print(new_list)
