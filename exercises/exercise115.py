f31531289fccb6afe8bced8c7549d35b34a69bce
djangojeng-e <headfat1218@gmail.com>
2020-03-08 18:28:07 +0900

    feat: Solution for Question 115 has been updated.

---
+++ exercises/exercise115.py
@@ -0,0 +1,13 @@
+#  The iterator protocol for Python declares that we must make
+# use of two function to build an iterator- iter() and next()
+
+
+# iter() - to Create an iterator
+# next() - to iterate to the next element
+
+a = iter([2,4,6,8,10])
+next(a)
+
+next(a)
+
+next(a)
