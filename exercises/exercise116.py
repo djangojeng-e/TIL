96a12b4d1ab15f9d9445d4480d55751674ac4b3e
djangojeng-e <headfat1218@gmail.com>
2020-03-09 10:42:32 +0900

    feat: Solution for Question 116 has been updated.

---
+++ exercises/exercise116.py
@@ -0,0 +1,13 @@
+# Tuple unpacking refers to unpacking values into the variables.
+
+# e.g.
+
+nums = (1, 2, 3)
+a, b, c = nums
+
+print(a)
+print(b)
+print(c)
+
+# We can see the values in tuples have been unpacked and
+# assigned to each variables a, b, c
