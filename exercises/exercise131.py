60ddae8f387372d09930359b4e1a62a6e67429b2
djangojeng-e <headfat1218@gmail.com>
2020-03-28 19:18:01 +0900

    feat: Solution for Question 131 has been udpated.

---
+++ exercises/exercise131.py
@@ -0,0 +1,10 @@
+# Explain join() and split() in Python
+
+# join() lets us join characters from a string together by a character we specify.
+
+print(','.join('12345'))
+
+# split() lets us split a string arund the character we specify
+
+print('1,2,3,4,5'.split(','))
+
