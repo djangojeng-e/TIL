931b5fa7f237b8aee113e672e21bda79d4aed31e
djangojeng-e <headfat1218@gmail.com>
2020-07-09 14:16:49 +0900

    JavaScript - array builtins reduce 2

---
+++ javascript/class_27_array_reduce_2.js
@@ -0,0 +1,14 @@
+// reduce 의 다른 예시
+
+const alphabets = ['a', 'a', 'a', 'b', 'c', 'c', 'd', 'e']
+
+const counts = alphabets.reduce((acc, current) => {
+  if (acc[current]) {
+    acc[current] += 1;
+  } else {
+    acc[current] = 1;
+  }
+  return acc;
+}, {})
+
+console.log(counts);
