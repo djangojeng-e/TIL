39ae26221212fbf43a15f2e94b555cbd5a803c94
djangojeng-e <headfat1218@gmail.com>
2020-08-13 15:55:40 +0900

    JavaScript - Class and prototype Introduction

---
+++ javascript/class_29_prototype_class.js
@@ -0,0 +1,31 @@
+// Prototype and class
+
+// 객체 생성자. object constructor
+
+function Animal(type, name, sound) {
+  this.type = type;
+  this.name = name;
+  this.sound = sound;
+
+}
+
+Animal.prototype.say = function() {
+  console.log(this.sound);
+}
+
+Animal.prototype.sharedValue = 1;
+
+const dog = new Animal('개', '멍멍이', '멍멍');
+const cat = new Animal('고양이', '야옹이', '야옹');
+
+
+dog.say();
+cat.say();
+
+
+console.log(dog.sharedValue);
+console.log(cat.sharedValue);
+
+
+// This is pretty much simialr to class in Python
+// Prototype can be shared throughout the instances of Animal 
