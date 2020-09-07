71f8b99a5925aa9338b7cd77aed86cc926e31ece
djangojeng-e <headfat1218@gmail.com>
2020-09-07 21:33:56 +0900

    JavaScript - Foundation revision started

---
+++ javascript/javascript_foundation/what_is_javascript.js
@@ -0,0 +1,14 @@
+// What is JavaScript?
+
+// JavaScript is a programming language
+
+// JavaScript is a Scripting language
+// - It is a interpreted language
+
+// Source codes are read line by line
+
+// Web browsers have scripting engines
+// JavaScript can change the action dynamically
+
+
+// Beyond the web browser, there are electron, react, nodejs, ionic, native script etc
