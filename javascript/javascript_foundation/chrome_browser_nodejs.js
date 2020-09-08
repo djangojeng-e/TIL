720bd44feae944ff655c0934db95648e1665d620
djangojeng-e <headfat1218@gmail.com>
2020-09-08 17:23:19 +0900

    JavaScript - practice environment

---
+++ javascript/javascript_foundation/chrome_browser_nodejs.js
@@ -0,0 +1,5 @@
+//Chrome Browser, node.js 대표적인 자바스크립트 실습 환경
+// 실습을 위해 chrome 사용
+
+
+console.log('Hello JavaScript')
---
+++ javascript/javascript_foundation/various_javascript_runtime.js
@@ -0,0 +1,9 @@
+// Various JavaScript Runtime
+
+// Chrome, Firefox, Safari, Edge etc
+// Node.js server environment
+
+// Electron is also JavaScript Runtime
+
+// ECMA Standard admission scripts
+// ES6 has been released
