62690c24aad5de6daa7dcbaf2b7efd7209ded1f1
djangojeng-e <headfat1218@gmail.com>
2020-09-09 14:28:35 +0900

    JavaScript - expression, statement, keywords, reserved words

---
+++ javascript/javascript_foundation/expression.js
@@ -0,0 +1,32 @@
+// expression (표현식)
+// 값을 만들어 내는 것
+
+// 예, true, 26
+// 1000 + 900 + 90 + 5 + 4  이는 값을 만들어 내는 수식이므로. 표현식으로 불린다
+
+// 표현식들은 값을 만들어 내기 때문에, 함수의 인자로 사용할수 있다.
+
+
+console.log(1000 + 900 + 90 + 4);
+
+// The result of console.log(1000 + 900 + 90 + 4) is the value of 1994
+
+// Statement (문장)
+
+// one or more expressions are gathered to gether to form a statement
+// all expression can be statement
+// Usually put semi-colon at the end of statement
+
+var name = "Mark";
+true;
+26;
+"David";
+'Hello' + 'World';
+
+console.log('Hello');
+
+
+// For one line statement, It is okay not to have semi colon at the end of sentence.
+// However, we use the semi colon as per conventional uses.
+
+// expression forms statement, statements to form program 
---
+++ javascript/javascript_foundation/keyword_and_reserved_words.js
@@ -0,0 +1,18 @@
+// Keywords & Reserved
+
+// Keywords
+// - 특정 목적을 위해 사용하는 단어
+// - 예약어로 지정되어 있음
+
+// e.g var, const, etc has the specific purposes that are keywords
+
+
+// Reserved words
+// The words are reserved for purposes already
+// these words cannot be used as values or variables
+
+// e.g.
+
+var return = 'name of variable'
+
+function for() {}
