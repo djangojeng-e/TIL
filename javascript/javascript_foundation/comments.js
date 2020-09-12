117e13511e28ac610d9c8a013802e07286c2dd9c
djangojeng-e <headfat1218@gmail.com>
2020-09-12 22:53:03 +0900

    JavaScript - Comments, identifiers

---
+++ javascript/javascript_foundation/comments.js
@@ -0,0 +1,13 @@
+// comments
+
+// 이 함수는 인사를 하는 함수 입니다 --> 이것이 바로 주석
+// the description comments can be started with //
+
+// multiple line commments
+
+/*
+This is the multi-line
+Comments
+starts with /*
+ends with */
+*/
---
+++ javascript/javascript_foundation/identifier.js
@@ -0,0 +1,27 @@
+// Identifier 식별자
+
+// 어떤 이름을 식별할수 있는 형태로 작성
+
+var name = 'Mark';
+function hell() {
+
+};
+
+var person = {name: 'Mark', age: 37};
+
+// Identifiers are case sensitive
+
+var myName = 'Mark';
+var myname = 'Mark';
+
+// these are different
+
+// identifiers can be unicode $, - 0 - 9 but it cannot start with numbers 0 - 9
+// reserved words and empty characters cannot be used
+
+
+var name1;
+var $name;
+var _name;
+
+var 이름 ; // this is possible but use English!
